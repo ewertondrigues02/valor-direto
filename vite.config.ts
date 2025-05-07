6d911b6ae806e8117a0909189dcc6f7408e472a1
Ewerton Rodrigues <106437473+ewertondrigues02@users.noreply.github.com>
2025-05-07 18:58:35 -0300

    site-criado

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const repoName = 'valor-direto'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [vue()],
 })
+
